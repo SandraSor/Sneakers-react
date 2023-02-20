d927a70a4b51bd97104143d45d235e05247673ae
SandraSor <soronastya@mail.ru>
2023-02-20 15:40:57 +0300

    Create Sneakers site + redesigned for components

---
+++ src/components/Drawer.js
@@ -0,0 +1,60 @@
+const Drawer = () => {
+  return (
+    <div style={{ display: "none" }} className="overlay">
+      <div className="drawer d-flex flex-column">
+        <h2 className="d-flex mb-30 justify-between">
+          Корзина
+          <img className="removeBtn" src="/img/btn-remove.svg" alt="" />
+        </h2>
+
+        <div className="items flex">
+          <div className="cartItem d-flex align-center mb-20">
+            <div
+              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+              className="cartItemImg"
+            ></div>
+            <div className="mr-20 flex">
+              <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
+              <b>12 990 руб.</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="" />
+          </div>
+
+          <div className="cartItem d-flex align-center mb-20">
+            <div
+              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+              className="cartItemImg"
+            ></div>
+            <div className="mr-20 flex">
+              <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
+              <b>12 990 руб.</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="" />
+          </div>
+        </div>
+
+        <div className="cartTotalBlock">
+          <ul>
+            <li>
+              <span>Итого: </span>
+              <div></div>
+              <b>21 498 руб. </b>
+            </li>
+            <li>
+              <span>Налог 5%:</span>
+              <div></div>
+              <b>1074 руб. </b>
+            </li>
+          </ul>
+
+          <button className="greenButton">
+            Оформить заказ
+            <img src="/img/arrow.svg" alt="Arrow" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Drawer;
